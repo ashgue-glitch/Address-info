b4f2f62eba1b82a05615d4023b17c9894fc0caaa
ashgue-glitch <ashley.guerra@csuglobal.edu>
2026-01-19 00:04:50 -0700

    Add files via upload

---
+++ AddressInfo.java
@@ -0,0 +1,14 @@
+package address_info;
+
+public class AddressInfo {
+
+	public static void main(String[] args) {
+		  System.out.println("First Name: ASH");
+	        System.out.println("Last Name: Dewild");
+	        System.out.println("Street Address: 123 LMNOP Street");
+	        System.out.println("City: Denver");
+	        System.out.println("Zip Code: 80203");
+
+	}
+
+}
